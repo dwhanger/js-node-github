7ed8607234ee3499faedb245e715a40b468c834c
Darren Whanger <dwhanger@hotmail.com>
2017-02-08 17:39:05 -0800

    node: the beginning

---
+++ Notes on node.js
@@ -0,0 +1,13 @@
+1. node app.js			-- to run a node program
+
+2. nodemon app.js		-- for continuous development purposes, picks up changes to files when they are saved
+
+3. npm install -g nodemon		-- to install nodemon, the -g flag installs nodmon globally so that it is available in all node programs
+								-- it will also add itself to the path so that it is available everywhere
+
+4. node app.js --debug			-- starts node with debug info enabled
+
+5. node-inspector		-- starts the debugging capabilities for node, app gets started on 8080
+						-- and the debugger is started on port 5858
+
+6. 
